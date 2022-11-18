20bd1670eb913e40bca855b8d2e51196cfd5215e
Ridgeso <nienekpalo@gmail.com>
2022-11-19 00:00:47 +0100

    Added GLFW as submodule

--- FileExplorer/src/FileExplorer/Core/Application.cpp
+++ FileExplorer/src/FileExplorer/Core/Application.cpp
@@ -1,3 +1,4 @@
+#include <GLFW/glfw3.h>
 #include "FileExplorer/Core/Application.h"
 
 namespace Explorer {
@@ -14,10 +15,31 @@ namespace Explorer {
 
     void Application::run()
     {
-        while (true)
-        {
+        GLFWwindow* window;
 
+        if (!glfwInit())
+            return;
+
+        window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+        
+        if (!window)
+        {
+            glfwTerminate();
+            return;
         }
+
+        glfwMakeContextCurrent(window);
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(window);
+
+            glfwPollEvents();
+        }
+        
+        glfwTerminate();
     }
 
 }
